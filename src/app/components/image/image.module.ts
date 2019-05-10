b02543581c63d7d715011fe9f2ac698c74edddbb
Alexander Yaroshevich <a_yaroshevich@indatalabs.com>
2019-05-10 19:11:04 +0300

    Modularize

--- src/app/components/image/image.module.ts
+++ src/app/components/image/image.module.ts
@@ -2,23 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ImageItemComponent } from './image-item/image-item.component';
 import { ImageListComponent } from './image-list/image-list.component';
-import { ImageDisplayComponent } from './image-display/image-display.component';
 import { CommonModule } from '@angular/common';
-import { ImagePreviewService } from './image-preview.service';
-import { SimilarImageService } from './similar-image.service';
-import { NeuralNetworkEvaluateService } from './neural-network-evaluate.service';
-import { ImageService } from './image.service';
 import { FormsModule } from '@angular/forms';
 import { PropertiesResolver, SettingsOptionsResolver } from './image-item/image-item-resolver.service';
-import { ImageSettingsComponent } from './image-item/image-settings/image-settings.component';
 import { UtilsModule } from '../utils/utils.module';
 import { MaterialModule } from '../../material.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { MatIconModule, MatTreeModule } from '@angular/material';
-import { ImageListTreeComponent } from './image-list/image-list-tree/image-list-tree.component';
-import { ImageListService } from './image-list/image-list.service';
-import { ImageListRecalculateModalComponent } from './image-list/image-list-recalculate-modal/image-list-recalculate-modal.component';
-import { ImageListRecalculateProgressModalComponent } from './image-list/image-list-recalculate-progress-modal/image-list-recalculate-progress-modal.component';
+import { ImageListModule } from './image-list/image-list.module';
+import { ImageItemModule } from './image-item/image-item.module';
 
 const routes: Routes = [
     { path: '', component: ImageListComponent },
@@ -34,13 +25,7 @@ const routes: Routes = [
 
 @NgModule({
     declarations: [
-        ImageItemComponent,
-        ImageListComponent,
-        ImageListTreeComponent,
-        ImageDisplayComponent,
-        ImageSettingsComponent,
-        ImageListRecalculateModalComponent,
-        ImageListRecalculateProgressModalComponent,
+
     ],
     imports: [
         RouterModule.forChild(routes),
@@ -49,21 +34,14 @@ const routes: Routes = [
         UtilsModule,
         MaterialModule,
         NgbModule,
-        MatTreeModule,
-        MatIconModule,
+        ImageItemModule,
+        ImageListModule,
     ],
     providers: [
-        ImageService,
-        ImagePreviewService,
-        SimilarImageService,
-        NeuralNetworkEvaluateService,
         SettingsOptionsResolver,
         PropertiesResolver,
-        ImageListService,
     ],
     entryComponents: [
-        ImageListRecalculateModalComponent,
-        ImageListRecalculateProgressModalComponent,
     ],
     exports: [RouterModule],
 })
